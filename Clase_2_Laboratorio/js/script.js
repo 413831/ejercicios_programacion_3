ad7592570cd9b6d9af83d61bf3aa80e78f7682c2
413831 <alepleins3pp@gmail.com>
2019-08-27 20:39:27 -0300

    Clase 2 Laboratorio

--- Clase_2_Laboratorio/js/script.js
+++ Clase_2_Laboratorio/js/script.js
@@ -1,3 +1,10 @@
+var btnSaludar; 
+
+window.addEventListener('load', function(){
+    btnSaludar = document.getElementById('btnSaludar');    
+    btnSaludar.addEventListener('click', saludar);
+}); 
+
 function saludar(){
     console.log("HOLUS");
     alert("HOLUS");
