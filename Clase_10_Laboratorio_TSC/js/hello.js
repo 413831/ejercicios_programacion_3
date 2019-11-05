bd25a3ad59333303484ebc838656668253d55c01
alumno <alumno@utnfra.com.ar>
2019-11-05 20:41:15 -0300

    Clase 10 TypeScript

--- Clase_10_Laboratorio_TSC/js/hello.js
+++ Clase_10_Laboratorio_TSC/js/hello.js
@@ -1,5 +1,8 @@
+"use strict";
 var ejemploUno = "Hola mundo";
 var ejemploDos = "Hola MUNDO";
 var mensaje;
 mensaje = "Hola Don Pepito";
+// Para traspilar => tsc hello.ts
+// Para configurar como proyecto TypeScript => tsc --init
 console.log(mensaje);
