740b54d0fa0bc87f424eeb03c20196a639fea5da
413831 <alepleins3pp@gmail.com>
2019-11-04 22:11:37 -0300

    Clase PHP / SQL / ORM

--- Full-API-REST-Skeleton/cdcol.sql
+++ Full-API-REST-Skeleton/cdcol.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-10-2016 a las 23:58:20
--- Versión del servidor: 5.6.21
--- Versión de PHP: 5.6.3
+-- Tiempo de generación: 05-11-2019 a las 02:11:14
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Base de datos: `cdcol`
@@ -26,21 +28,23 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `cds`
 --
 
-CREATE TABLE IF NOT EXISTS `cds` (
+CREATE TABLE `cds` (
   `titel` varchar(200) COLLATE latin1_general_ci DEFAULT NULL,
   `interpret` varchar(200) COLLATE latin1_general_ci DEFAULT NULL,
   `jahr` int(11) DEFAULT NULL,
-`id` bigint(20) unsigned NOT NULL
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
 -- Volcado de datos para la tabla `cds`
 --
 
-INSERT INTO `cds` (`titel`, `interpret`, `jahr`, `id`) VALUES
-('Beauty', 'Ryuichi Sakamoto', 1990, 1),
-('Goodbye Country (Hello Nightclub)', 'Groove Armada', 2001, 4),
-('Glee', 'Bran Van 3000', 1997, 5);
+INSERT INTO `cds` (`titel`, `interpret`, `jahr`, `id`, `created_at`, `updated_at`) VALUES
+('Beauty', 'Ryuichi Sakamoto', 1990, 1, '2019-11-05 01:10:01', '0000-00-00 00:00:00'),
+('Goodbye Country (Hello Nightclub)', 'Groove Armada', 2001, 4, '2019-11-05 01:10:01', '0000-00-00 00:00:00'),
+('Glee', 'Bran Van 3000', 1997, 5, '2019-11-05 01:10:01', '0000-00-00 00:00:00');
 
 --
 -- Índices para tablas volcadas
@@ -50,7 +54,7 @@ INSERT INTO `cds` (`titel`, `interpret`, `jahr`, `id`) VALUES
 -- Indices de la tabla `cds`
 --
 ALTER TABLE `cds`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -60,7 +64,9 @@ ALTER TABLE `cds`
 -- AUTO_INCREMENT de la tabla `cds`
 --
 ALTER TABLE `cds`
-MODIFY `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
